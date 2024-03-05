bcd97ed5a16181c0d2e1f23715fbaa74450a3608
digi2solution <161964480+digi2solution@users.noreply.github.com>
2024-03-06 00:19:03 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+  return (
+    <>
+   
+    <h1 className="text-2xl md:text-4xl lg:text-6xl font-medium  tracking-wide">Welcome to Digital agency 🚀</h1>
+    </>
+  )
+}
+
+export default App
