2ecdc01c3e92a5ec7bde5bcdf268d0e07a672fa0
Zaahiid <zahidrsheikh01@gmail.com>
2024-03-06 00:57:21 +0530

    Testing Cloudflare integration

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ function App() {
     <>
    
     <h1 className="text-2xl md:text-4xl lg:text-6xl font-medium  tracking-wide">Welcome to DigiTwo Solutions 🚀</h1>
+    <h2>Testing Cloudflare Integration 🙂</h2>
     </>
   )
 }
